a4c167a88ac85bd063e238f48e3f350f871bfb9f
steventd96 <steventheodorus@yahoo.com.sg>
2019-07-04 16:35:15 -0400

    concat subchild

--- NSFparsing.py
+++ NSFparsing.py
@@ -4,24 +4,37 @@ import pandas as pd
 import csv
 
 '20**/*.xml'
-def pase(string):
+def parse(string):
     files = glob.glob(string)
     df_main = pd.DataFrame()
     for file in files:
-        print(file)
         tree = ET.parse(file)
         root = tree.getroot()
         full_tag_list = []
         tag_list  = []
         text_list = []
         df = pd.DataFrame()
+        save_null_tag = ''
         for elem in root.iter():
-            tag_list.append(elem.tag)
-            text_list.append(elem.text)
-        print(tag_list)
+            if elem.tag != 'rootTag' and elem.tag != 'Award':
+                tag = elem.tag
+                text = elem.text
+                if text == '\n':
+                    save_null_tag = tag
+                else:
+                    if save_null_tag != '':
+                        concat_tag = save_null_tag + ' ' + tag
+                        tag_list.append(concat_tag)
+                        text_list.append(elem.text)
+                    else:
+                        tag_list.append(elem.tag)
+                        text_list.append(elem.text)
+        print(len(tag_list))
+        print(len(text_list))
         df = pd.DataFrame(text_list).T
         df.columns = tag_list
-        df_main = pd.concat([df_main,df])
+        df_main = pd.concat([df_main,df],axis = 0, ignore_index = True)
+        #print(df_main)
 
 def get_tags(string):
     files = glob.glob(string)
