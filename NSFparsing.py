947a3e23bea230407974ca740e0928d5871e9710
steventd96 <steventheodorus@yahoo.com.sg>
2019-07-08 08:31:40 -0400

    initial processing

--- NSFparsing.py
+++ NSFparsing.py
@@ -4,25 +4,33 @@ import pandas as pd
 import csv
 import re
 
+def findNumOfSubChild(child):
+    i = 0
+    for subchild in child.iter():
+        i = i+1
+    return i
+
 '20**/*.xml'
 def parse(string):
     files = glob.glob(string)
     df_main = pd.DataFrame()
-    i =0
     for file in files:
-        print(file)
         try:
             tree = ET.parse(file)
-        except ET.ParseError:
+        except ET.ParseError as e:
+            print(e)
+            print(file)
+            continue
+            '''
             with open(file, 'a') as f:
                 f.write('</Award>\n')
                 f.write('</rootTag>\n')
             tree = ET.parse(file)
+            '''
         root = tree.getroot()
         tag_list  = []
         text_list = []
         df = pd.DataFrame()
-        save_null_tag = ''
         duplicate_tag_count = {}
         # Iterate through each elements 
         for elem in root.iter():
@@ -30,39 +38,20 @@ def parse(string):
             if elem.tag != 'rootTag' and elem.tag != 'Award':
                 tag = elem.tag
                 text = elem.text
-                # If text is null, that means it has a subchild. Save the tag 
-                if text == '\n':
-                    save_null_tag = tag
+                if tag in tag_list:
+                    try:
+                        duplicate_tag_count[tag] = duplicate_tag_count[tag] +1
+                    except KeyError:
+                        duplicate_tag_count[tag] = 1
+                    tag_list.append(tag + '_' + str(duplicate_tag_count[tag]))
+                    text_list.append(elem.text)
                 else:
-                    # If save_null_tag is not '', concat the tag 
-                    if save_null_tag != '':
-                        concat_tag = save_null_tag + ' ' + tag
-                        if concat_tag in tag_list:
-                            try:
-                                duplicate_tag_count[concat_tag] = duplicate_tag_count[concat_tag] +1
-                            except KeyError:
-                                duplicate_tag_count[concat_tag] = 1
-                            tag_list.append(save_null_tag + '_' + str(duplicate_tag_count[concat_tag])+ ' ' +tag)
-                            text_list.append(elem.text)
-                        else:
-                            tag_list.append(concat_tag)
-                            text_list.append(elem.text)
-                    # If save_null_tag is '', 
-                    else:
-                        if tag in tag_list:
-                            try:
-                                duplicate_tag_count[concat_tag] = duplicate_tag_count[concat_tag] +1
-                            except KeyError:
-                                duplicate_tag_count[concat_tag] = 0
-                            tag_list.append(save_null_tag + '_' + str(duplicate_tag_count[concat_tag])+ ' ' +tag)
-                            text_list.append(elem.text)
-                        else:
-                            tag_list.append(elem.tag)
-                            text_list.append(elem.text)
+                    tag_list.append(elem.tag)
+                    text_list.append(elem.text)
+
         df = pd.DataFrame(text_list).T
         df.columns = tag_list
-        df_main = df_main.append(df)
-        i = i +1
+        df_main = df_main.append(df, sort=False)
     df_main.to_csv(string[:4] + '_data.csv')
 
 
